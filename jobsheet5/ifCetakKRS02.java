e1ed4a904fc9c3058160561d6c5f7c9874348ba6
alyafakhrunnisa <nisafakhrunalya@gmail.com>
2025-10-01 09:18:25 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS02.java
+++ jobsheet5/ifCetakKRS02.java
@@ -1,16 +1,20 @@
-package PraktikumDaspro.jobsheet5;
+package jobsheet5;
 import java.util.Scanner;
+
 public class ifCetakKRS02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("--- Cetak KRS SIAKAD ---");
-        System.out.println("Apakah UKT sudah lunas? (true/false): ");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
         if(uktLunas){
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Pembayaran UKT belum terverifikasi");
+            System.out.println("Silakan selesaikan pembayaran UKT terlebih dahulu");
         }
 
     }
