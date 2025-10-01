c491059d49605dd9e7a62ba58e4832ebca544db3
alyafakhrunnisa <nisafakhrunalya@gmail.com>
2025-10-01 09:40:39 +0700

    Modifikasi Percobaan 2

--- jobsheet5/ifElseCetakKRS02.java
+++ jobsheet5/ifElseCetakKRS02.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ifElseCetakKRS02 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
 
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.print("Masukkan semester saat ini : ");
